ef8e64622b5b3837a11d659e2afc7996a6887745
Donovan Palmer <donovanpalmer061@gmail.com>
2020-09-23 17:54:25 -0400

    phase 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from "react"
+import TodoItem from "./components/TodoItem.js"
 
 function App() {
     return (
-        <div>
-                <input type="checkbox" />
-                <p>Placeholder text here</p>
-
-                <input type="checkbox" />
-                <p>Placeholder text here</p>
-
-                <input type="checkbox" />
-                <p>Placeholder text here</p>
-
-                <input type="checkbox" />
-                <p>Placeholder text here</p>
+        <div className="todo-list">
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
         </div>
-
     )
 }
 
